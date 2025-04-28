55e097cdbf6e860d477aa18ff1b53d2816f6b12d
lucag316 <lucafranco316@gmail.com>
2025-04-28 14:41:07 -0300

    Creando estructura de directorios y haciendo el login

---
+++ sala-de-juegos/src/app/pages/login/login.component.ts
@@ -0,0 +1,46 @@
+
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  imports: [FormsModule, CommonModule, RouterModule], //pongo el FormsModule aca sino me tira error en el HTML ngForm, el RouterModule es para que me funcione el routerLink en el html
+})
+export class LoginComponent {
+
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private router: Router) {}
+
+  /**
+   * Maneja el proceso de login del usuario.
+   * 
+   * Si las credenciales (usuario y contraseña) son correctas,
+   * redirige al usuario a la página principal (/home).
+   * Si son incorrectas, muestra un mensaje de error en pantalla.
+  */
+  onLogin(){
+    //Verifica si el usuario y la contraseña son 'a'
+    if (this.username === 'a' && this.password === 'a') {
+      // Muestra un mensaje en la consola (sólo para desarrollo)
+      console.log('Login exitoso');
+    
+      // Limpia cualquier mensaje de error previo
+      this.errorMessage = '';
+
+      // Redirige al usuario a la página principal 'home'
+      this.router.navigate(['/home']);
+
+    } else {
+      // Si las credenciales son incorrectas, muestra mensaje de error
+      this.errorMessage = 'Usuario o contraseña incorrectos';
+    }
+  }
+
+}
