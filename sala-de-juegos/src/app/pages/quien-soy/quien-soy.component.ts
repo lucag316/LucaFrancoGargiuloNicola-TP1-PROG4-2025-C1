f0c9514d4d1cea58127696fea51cffdcd77b9cc4
lucag316 <lucafranco316@gmail.com>
2025-04-28 19:48:14 -0300

    Quien soy funcionando

--- sala-de-juegos/src/app/pages/quien-soy/quien-soy.component.ts
+++ sala-de-juegos/src/app/pages/quien-soy/quien-soy.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+// interfaz de usuario de github 
+interface GitHubUser{
+  login: string;
+  avatar_url: string;
+  name: string;
+  bio: string;
+  location: string;
+  followers: number;
+  following: number;
+  public_repos: number;
+}
 
 @Component({
   selector: 'app-quien-soy',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quien-soy.component.html',
-  styleUrl: './quien-soy.component.css'
+  styleUrl: './quien-soy.component.css',
+  standalone: true
 })
-export class QuienSoyComponent {
+
+
+export class QuienSoyComponent implements OnInit {
+
+  userData = signal<GitHubUser | null>(null);
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchGitHubData();
+  }
+
+  fetchGitHubData() {
+    this.http.get<GitHubUser>('https://api.github.com/users/lucag316')
+      .subscribe({
+        next: (data) => {
+          this.userData.set(data);
+        },
+        error: (error) => {
+          console.error("ERROR fetching GitHub data:", error);
+        }
+      });
+  }
 
 }
